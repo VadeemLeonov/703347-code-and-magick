b1c2e19e184e7774dd364cf519cf4f82e0da5202
VadeemLeonov <leonov.va@bk.ru>
2018-11-24 22:14:50 +0300

    справил цвета гистограммы

--- js/stat.js
+++ js/stat.js
@@ -6,14 +6,17 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP_BIG = 50;
 var GAP_SMALL = 10;
-var CONGRATULATIONS_GAP = CLOUD_HEIGHT - GAP_BIG;
 var BAR_WIDTH = 40;
-var barHeight = CLOUD_HEIGHT - GAP_BIG * 2 - GAP_SMALL * 2;
+var congratulationsGap = CLOUD_HEIGHT - GAP_BIG;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
+
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * ((max + 1) - min)) + min;
+}
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -38,14 +41,17 @@ window.renderStatistics = function (ctx, players, times) {
 
   var maxTime = getMaxElement(times);
   for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = 'hsl(240, 100%, 50%)';
+    ctx.fillStyle = 'hsl(240, ' + getRandomInt(0, 100) + '%, 50%)';
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP_BIG + (GAP_BIG + BAR_WIDTH) * i, CLOUD_Y + CONGRATULATIONS_GAP - (barHeight * times[i]) / maxTime);
-    ctx.fillText(players[i], CLOUD_X + GAP_BIG + (GAP_BIG + BAR_WIDTH) * i, CLOUD_Y + CONGRATULATIONS_GAP + GAP_SMALL);
-    ctx.fillRect(CLOUD_X + GAP_BIG + (GAP_BIG + BAR_WIDTH) * i, CLOUD_Y + CONGRATULATIONS_GAP, BAR_WIDTH, GAP_SMALL * 2 - (barHeight * times[i]) / maxTime);
+    var barChartX = CLOUD_X + GAP_BIG + (GAP_BIG + BAR_WIDTH) * i;
+    var barChartY = CLOUD_Y + congratulationsGap;
+    var barHeight = (CLOUD_HEIGHT - GAP_BIG * 2 - GAP_SMALL * 2) * times[i] / maxTime;
 
+    ctx.fillText(Math.round(times[i]), barChartX, barChartY - barHeight);
+    ctx.fillText(players[i], barChartX, barChartY + GAP_SMALL);
+    ctx.fillRect(barChartX, barChartY, BAR_WIDTH, GAP_SMALL * 2 - barHeight);
   }
 };
 
